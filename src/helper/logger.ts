f4e51ee59367908bfc5452e5244038e2955b100c
Tijan <ayomidetijani6@gmail.com>
2023-05-09 07:25:02 +0100

    Logger implemented: cleaner code

---
+++ src/helper/logger.ts
@@ -0,0 +1,28 @@
+import winston from 'winston';
+
+export const LOG_CONSOLE = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+const levels = {
+  error: 'error',
+  warn: 'warn',
+  info: 'info',
+  http: 'http',
+  verbose: 'verbose',
+  debug: 'debug',
+  silly: 'silly',
+};
+
+export const logger = {
+  error: (message: string) => LOG_CONSOLE.log({ level: levels.error, message }),
+  warn: (message: string) => LOG_CONSOLE.log({ level: levels.warn, message }),
+  info: (message: string) => LOG_CONSOLE.log({ level: levels.info, message }),
+  http: (message: string) => LOG_CONSOLE.log({ level: levels.http, message }),
+  verbose: (message: string) => LOG_CONSOLE.log({ level: levels.verbose, message }),
+  debug: (message: string) => LOG_CONSOLE.log({ level: levels.debug, message }),
+  silly: (message: string) => LOG_CONSOLE.log({ level: levels.silly, message }),
+};

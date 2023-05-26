a93dc69df887906a1a114f20b568fcb937791874
TijanAyo <ayomidetijani6@gmail.com>
2023-05-26 15:12:32 -0700

    Pre-planning: Providing more context for the wallet logic

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -1,5 +1,32 @@
 class WalletController {
 
+    /**
+     * Method: POST and PATCH
+     * Endpoint: /dashboard/wallet/fund
+     * Description: Handles request to fund users wallet
+     */
+    public async fundWallet() {
+
+    }
+
+    /**
+     * Method: GET
+     * Endpoint: /dashboard/wallet/balance
+     * Description: Retrieves the current users balance
+     */
+    public async walletBalance() {
+
+    }
+
+    /**
+     * Method: PATCH
+     * Endpoint: /dashboard/save
+     * Description: Allows user to save money from their wallet
+     */
+    public async saveFromWallet() {
+
+    }
+
 }
 
 export default WalletController;
--- src/service/wallet.service.ts
+++ src/service/wallet.service.ts
@@ -1,6 +1,28 @@
 class WalletService {
 
-    
+    /**
+     * Endpoint: /dashboard/wallet/fund
+     * Description: Handles request to fund users wallet
+     */
+    public async fundWallet() {
+
+    }
+
+    /**
+     * Endpoint: /dashboard/wallet/balance
+     * Description: Retrieves the current users balance
+     */
+    public async walletBalance() {
+
+    }
+
+    /**
+     * Endpoint: /dashboard/save
+     * Description: Allows user to save money from their wallet
+     */
+    public async saveFromWallet() {
+
+    }
 
 }
 

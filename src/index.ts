4081ca04c67eb1829bfaf946e0df3e1fbd097a02
TijanAyo <ayomidetijani6@gmail.com>
2023-06-01 23:40:25 +0100

    wallet route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import * as dotenv from "dotenv";
 import authRoute from "./routes/auth.route";
 import profileRoute from "./routes/profile.route";
+import walletRoute from "./routes/wallet.route";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.urlencoded({extended: false}));
 
 app.use("/auth", authRoute);
 app.use("/profile", profileRoute);
+app.use("/dashboard/wallet", walletRoute);
 
 app.get('/', (_req: Request, res: Response) => {
     return res.status(200).json({statusCode: 200, message: "PennyWiseAPI V1.0"});

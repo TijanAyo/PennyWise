e0afcf25ce2bab02f73741a1ee0cb17ba6f900f7
TijanAyo <ayomidetijani6@gmail.com>
2023-06-01 23:41:12 +0100

    get balance route

--- src/routes/wallet.route.ts
+++ src/routes/wallet.route.ts
@@ -1,5 +1,11 @@
 import express from "express";
-const router = express.Router();
+import WalletController from "../controllers/wallet.controller";
+import AuthMiddleware from "../middleware/auth.middleware";
 
+const router = express.Router();
+const walletController = new WalletController();
+const authMiddleware = new AuthMiddleware();
+
+router.get("/balance", authMiddleware.authorize, walletController.walletBalance);
 
 export default router;
